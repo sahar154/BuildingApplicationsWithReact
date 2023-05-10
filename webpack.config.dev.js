79b5f9275a00552587e1d79e9e7abf3f99885237
Sahar.unq <sahar@unq.co.il>
2023-05-10 09:21:41 +0300

    add build for dev for camparison

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -10,9 +10,9 @@ module.exports = {
   devtool: "cheap-module-source-map",
   entry: "./src/index",
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "buildDev"),
     publicPath: "/",
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   devServer: {
     stats: "minimal",
@@ -20,28 +20,28 @@ module.exports = {
     historyApiFallback: true,
     disableHostCheck: true,
     headers: { "Access-Control-Allow-Origin": "*" },
-    https: false
+    https: false,
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.API_URL": JSON.stringify("http://localhost:3001")
+      "process.env.API_URL": JSON.stringify("http://localhost:3001"),
     }),
     new HtmlWebpackPlugin({
       template: "src/index.html",
-      favicon: "src/favicon.ico"
-    })
+      favicon: "src/favicon.ico",
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader", "eslint-loader"]
+        use: ["babel-loader", "eslint-loader"],
       },
       {
         test: /(\.css)$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  }
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
